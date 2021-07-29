1573f75b2c4bac73a7a9d56bf31b143b0ca87bac
Peter Labick <Plabick@users.noreply.github.com>
2021-07-29 16:17:43 -0400

    Update conduct-speedtest.py

--- resources/conduct-speedtest.py
+++ resources/conduct-speedtest.py
@@ -26,8 +26,20 @@ def bits_to_megabits(bits):
     bits_per_megabit = 1000000
     return bits / bits_per_megabit
 
+
+def metric_badge(label, metric, unit):
+    return f"![badge](https://img.shields.io/badge/{label}-{metric}{unit}-blue?style=flat-square&logo=appveyor)"
+
 if __name__ == "__main__":
     file = sys.argv[1]
     current_time = int(sys.argv[2])
     current_speed = conduct_network_speed_test()
     add_to_history_file(file, current_speed, current_time)
+
+    upload_badge = metric_badge("Upload%20Speed", current_speed[0], "%20Mb%2Fs")
+    download_badge = metric_badge("Download%20Speed", current_speed[1], "%20Mb%2Fs")
+    ping_badge = metric_badge("Ping", current_speed[2], "%20ms")
+    
+    print(f"::set-output name=upload::{upload_badge}")
+    print(f"::set-output name=download::{download_badge}")
+    print(f"::set-output name=ping::{ping_badge}")
